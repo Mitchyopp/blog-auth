bc30a229b361cb2ea57837b4b949eb53076d8a05
Mitchy <MitchoppDEV@gmail.com>
2025-11-15 01:47:32 +0000

    finished the index.js tho ive needed some tutorials for this i need more practice with sql

--- src/models/index.js
+++ src/models/index.js
@@ -3,3 +3,15 @@ const sequelize = require('../config/db');
 const User = require('./User.js')(sequelize);
 const Post = require('./Post.js')(sequelize);
 const Category = require('./Category.js')(sequelize);
+
+User.hasMany(Post, { foreignKey: 'userId' });
+Post.belongsTo(User, { foreignKey: 'userId' });
+Category.hasMany(Post, { foreignKey: 'categoryId' });
+Post.belongsTo(Category, { foreignKey: 'categoryId' });
+
+module.exports = {
+  sequelize,
+  User,
+  Post,
+  Category,
+};
