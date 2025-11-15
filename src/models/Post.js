741590642a99c093816e1ba3c02130731f608cfc
Mitchy <MitchoppDEV@gmail.com>
2025-11-15 01:42:33 +0000

    added the post.js Model, neovim makes copying segments so easy

---
+++ src/models/Post.js
@@ -0,0 +1,20 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+  const Post = sequelize.define('Post', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    }
+  });
+  return Post;
+};
